b977c6c2b07abfd607f5282bb7dec7361ab806d1
Nguyễn Hiếu <hieurio12@gmail.com>
2023-11-09 10:21:58 +0700

    complete logic for layout 3 and layout 4

--- components/Screen02/index.js
+++ components/Screen02/index.js
@@ -1,25 +1,44 @@
 import { View, Text, StatusBar, SafeAreaView, Image, TextInput, FlatList, Pressable, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Screen02({navigation, route}) {
     const [search, setSearch] = useState("");
     const [todoList, setTodoList] = useState([]);
+    const [data, setData] = useState([]);
+    const [isUpdateList, setIsUpdateList] = useState(false)
+    useEffect(()=>{
+        const getData = async ()=>{
+            const res = await fetch("https://n38s2n-3000.csb.app/todo_list_tuan7?username="+route?.params?.username);
+            const dataArr = await res.json();
+            setData(dataArr);
+        }
+        getData();
+    }, [isUpdateList])
+
     const renderItemTodo = (item, index)=>{
         return (
             <View style={{backgroundColor: "#ccc", borderRadius: 25, flexDirection:"row", marginBottom: 25
-            , paddingHorizontal: 25, alignItems: "center"}}>
+            , paddingHorizontal: 25, alignItems: "center", justifyContent: "space-between", paddingVertical: 3}}>
                 <Image
                     source={require("../../assets/check.png")}
                     resizeMode='contain'
                     style={{width: 25, height: 25}}
                 />
-                <Text style={{fontWeight: "500", fontSize: 16, marginLeft: 12, flexGrow: 1, paddingVertical: 15}}>{item}</Text>
+                <View style={{flex: 1}}>
+                    <Text style={{fontWeight: "500", fontSize: 16, marginLeft: 12, flexGrow: 1, paddingVertical: 5
+                    , color: item.priority === 1 ? "red" : item.priority === 2 ? "orange" : "green"}}
+                    >{item.todo_name}</Text>
+                    <Text style={{marginLeft: 12, fontSize: 16}}>Hạn: {item.date}</Text>
+                </View>
                 <Pressable
                     onPress={()=>navigation.navigate("Screen03", {
                         action: "update",
                         username: route?.params?.username,
                         data: item,
-                        index: index
+                        index: index,
+                        isUpdateList: isUpdateList,
+                        setIsUpdateList: setIsUpdateList
                     })}
                 >
                     <Image
@@ -35,18 +54,8 @@ export default function Screen02({navigation, route}) {
     const handleSetSeach = (text)=>{
         setSearch(text);
     }
-   useEffect(()=>{
-        if (route?.params?.action == "add"){
-           
-            setTodoList([...todoList, route.params.todo]);
-        } else if (route?.params?.action == "update"){
-            let newArr = [...todoList];
-            newArr[route?.params?.index] =  route?.params?.todo;
-            setTodoList(newArr);
-        }
-   }, [route?.params?.todo])
 
-   console.log(route.params)
+
   return (
     <View style={{flex: 1}}>
         <StatusBar/>
@@ -59,15 +68,11 @@ export default function Screen02({navigation, route}) {
                         <Pressable
                             onPress={()=> navigation.goBack()}
                         >
-                            <Image
-                                source={require("../../assets/back.jpg")}
-                                resizeMode='contain'
-                                style={{width: 25, height: 25}}
-                            />
+                            <Ionicons name="arrow-back-outline" size={28} color="black" />
                         </Pressable>
                         <View style={{flexDirection: "row" ,alignItems :"center"}}>
                             <Image
-                                source={require("../../assets/avatar.jpg")}
+                                source={require("../../assets/avatarmain.png")}
                                 style={{width: 45, height: 45, borderRadius: 100}}
                                 resizeMode='contain'
                             />
@@ -79,16 +84,17 @@ export default function Screen02({navigation, route}) {
                         </View>
                     </View>
                 </View>
-                <View style={{flexDirection: "row", borderWidth: 1, borderRadius: 4}}>
+                <View style={{flexDirection: "row", borderWidth: 1, borderRadius: 4, alignItems: "center"}}>
                     <Image
-                        source={require("../../assets/search.jpg")}
-                        style={{width: 35, height: 35}}
+                        source={require("../../assets/search-image.png")}
+                        style={{width: 30, height: 30, marginHorizontal: 10}}
                         resizeMode='contain'
                     />
                     <TextInput
                         placeholder='Search'
-                        placeholderTextColor="#ccc"
-                        style={{flexGrow: 1, paddingVertical: 10}}
+                        placeholderTextColor="#333"
+
+                        style={{flexGrow: 1, paddingVertical: 10, fontWeight: "500", fontSize: 16}}
                         value={search}
                         onChangeText={(text)=>handleSetSeach(text)}
                     />
@@ -96,7 +102,7 @@ export default function Screen02({navigation, route}) {
                 <View>
                     <FlatList
                         style={{marginTop: 50}}
-                        data={search != "" ? todoList.filter(item => item.includes(search)) : todoList }
+                        data={search != "" ? data.filter(item => item.todo_name.includes(search)) : data }
                         keyExtractor={(item, index)=> index +""}
                         renderItem={({item, index})=> renderItemTodo(item, index)}
                     />
@@ -104,7 +110,9 @@ export default function Screen02({navigation, route}) {
                 <Pressable style={{alignItems: "center"}}
                     onPress={()=> navigation.navigate("Screen03", {
                         action: "add",
-                        username: route?.params?.username
+                        username: route?.params?.username, 
+                        isUpdateList: isUpdateList,
+                        setIsUpdateList: setIsUpdateList
                     })}
                 >
                     <Image
--- components/Screen03/index.js
+++ components/Screen03/index.js
@@ -1,112 +1,215 @@
 import { View, Text, StatusBar, ScrollView, Pressable, Image, TextInput } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { ActivityIndicator } from 'react-native';
+
+const priorities = [
+    {
+        level: 1,
+        color: "red",
+    },
+    {
+        level: 2,
+        color: "orange",
+    },
+    {
+        level: 3,
+        color: "green"
+    }
+]
 
 export default function Screen03({navigation, route}) {
-    const [textInput, setTextInput] = useState("");
-    const handleClickFinish = ()=>{
+    const {isUpdateList, setIsUpdateList} = route.params;
+    //route.params?.data?.todo_name ||
+    const [textInput, setTextInput] = useState(route.params?.data?.todo_name || "");
+    const [isSubmit, setIsSubmit] = useState(false);
+    //route.params?.data?.date ? new Date(route.params.data.date) : 
+    const [date, setDate] = useState(route.params?.data?.date ? new Date(route.params.data.date) : new Date());
+    const [showPicker, setShowPicker] = useState(false);
+    const [priority, setPriority] = useState(route.params?.data?.priority || priorities[0].level);
+
+    useEffect(()=>{
+        if (route.params?.action == "update"){
+            setDate(new Date(route.params?.data?.date));
+            setPriority(route.params?.data?.priority);
+            setTextInput(route.params?.data?.todo_name);
+        }
+    }, [route.params])
+
+    const handleClickFinish = async ()=>{
         if (textInput == ""){
             return;
         }
         if (route.params?.action == "add"){
             
-            navigation.navigate("Screen02", {
-                action: "add",
-                todo: textInput,
-                username: route?.params?.username
-            })
+            setIsSubmit(true);
+            const res = await fetch("https://n38s2n-3000.csb.app/todo_list_tuan7", { 
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username: route?.params?.username,
+                    todo_name: textInput,
+                    date: date.toISOString().slice(0, 10),
+                    priority: priority
+                })
+
+            }); 
+            setIsUpdateList(!isUpdateList);
+
+            setIsSubmit(false);
             setTextInput("");
-        } else {
             navigation.navigate("Screen02", {
-                action: "update",
-                todo: textInput,
                 username: route?.params?.username,
-                index: route?.params?.index
-            })
+            });
+        
+        } else {
+        
+            setIsSubmit(true);
+            const res = await fetch(`https://n38s2n-3000.csb.app/todo_list_tuan7/${route.params?.data?.id}`, { 
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username: route?.params?.username,
+                    todo_name: textInput,
+                    date: date.toISOString().slice(0, 10),
+                    priority: priority
+                })
+            });
+            setIsSubmit(false);
             setTextInput("");
+            setIsUpdateList(!isUpdateList);
+            navigation.navigate("Screen02", {
+                username: route?.params?.username,
+            });
         }
     }
 
-    useEffect(()=>{
-        if (route.params?.action == "update"){
-            setTextInput(route.params?.data)
-        }
-    }, [route.params?.action, route.params?.data])
-  return (
-    <View style={{flex: 1, paddingHorizontal: 30}}>
-        <StatusBar/>
-        <SafeAreaView style={{flex: 1}}>
-            <ScrollView stickyHeaderIndices={[0]}>
+    return (
+        <View style={{flex: 1}}>
+            <StatusBar/>
+            <SafeAreaView style={{flex: 1}}>
+                <ScrollView stickyHeaderIndices={[0]}>
 
-                <View>
-                        <View style={{flexDirection: "row", justifyContent: "space-between", paddingVertical: 15}}>
-                            <View style={{flexDirection: "row" ,alignItems :"center"}}>
-                                <Image
-                                    source={require("../../assets/avatar.jpg")}
-                                    style={{width: 45, height: 45, borderRadius: 100}}
-                                    resizeMode='contain'
-                                />
-                                <View>
-                                    <Text style={{textAlign: "center", fontWeight: "700", }}>Hi {route.params?.username}</Text>
-                                    <Text style={{marginLeft: 4, fontWeight: "500", color: "#666"}}>Have a greate day a head</Text>
+                    <View style={{backgroundColor:"#fff"}}>
+                            <View style={{flexDirection: "row", justifyContent: "space-between", paddingVertical: 15, zIndex: 2}}>
+                                <View style={{flexDirection: "row" ,alignItems :"center"}}>
+                                    <Image
+                                        source={require("../../assets/avatarmain.png")}
+                                        style={{width: 45, height: 45, borderRadius: 100}}
+                                        resizeMode='contain'
+                                    />
+                                    <View>
+                                        <Text style={{textAlign: "center", fontWeight: "700", }}>Hi {route.params?.username}</Text>
+                                        <Text style={{marginLeft: 4, fontWeight: "500", color: "#666"}}>Have a greate day a head</Text>
+                                    </View>
+
                                 </View>
-
+                                <Pressable
+                                    onPress={()=> navigation.goBack()}
+                                >
+                                    <Ionicons name="arrow-back-outline" size={28} color="black" />
+                                </Pressable>
+                            
                             </View>
-                            <Pressable
-                                onPress={()=> navigation.goBack()}
-                            >
-                                <Image
-                                    source={require("../../assets/back.jpg")}
-                                    resizeMode='contain'
-                                    style={{width: 25, height: 25}}
-                                />
-                            </Pressable>
-                           
+                    </View>
+
+                    <View style={{alignItems: "center", marginTop: 40, marginHorizontal: 30}}>
+                        <Text style={{fontSize: 35, fontWeight: "700"}}>{route.params?.action == "add" ? "ADD YOUR JOB" : "EDIT YOUR JOB"}</Text>
+
+                    </View>
+
+                    <View style={{flexDirection: "row", alignItems: "center", borderWidth: 1, paddingVertical: 8, marginHorizontal: 30
+                    ,borderRadius: 4, marginTop:30}}>
+                        <Image
+                            source={require("../../assets/notelist.png")}
+                            resizeMode='contain'
+                            style={{width: 25, height: 25, marginHorizontal: 10}}
+                        />
+                        <TextInput
+                            placeholder='input your job'
+                            style={{flexGrow: 1, fontSize: 16}}
+                            placeholderTextColor={"#333"}
+                            value={textInput}
+                            onChangeText={setTextInput}
+                        />
+                    </View>
+                
+                    <Pressable style={{flexDirection: "row", alignItems: "center", borderWidth: 1, paddingVertical: 8, marginHorizontal: 30
+                    ,borderRadius: 4, marginTop:30}}
+                        onPress={()=> setShowPicker(true)}
+                    >
+                        <Text style={{textAlign: "center", width: "100%", fontSize: 16}}>{date.toISOString().slice(0, 10)}</Text>
+                    </Pressable>
+
+
+                    {
+                            showPicker 
+                            &&
+                            <DateTimePicker
+                                mode='date'
+                                display='spinner'
+                                value={date}
+                                onChange={(event, selectedDate)=>{setDate(selectedDate); setShowPicker(false)}}
+                            />
+                    }
+
+                
+                    <View style={{width: "100%", marginTop: 20, marginHorizontal: 30}}>
+                        <Text style={{fontSize: 20, fontWeight: "500"}}>Priority</Text>
+                        <View style={{width: "100%", flexDirection: "row", justifyContent: "space-evenly", marginTop: 5}}>
+                            {
+                                priorities.map((item, index)=>{
+                                    return (
+                                        <Pressable key={index}
+                                            onPress={()=> setPriority(item.level)}
+                                            style={{width: 40, height: 40, justifyContent: "center"
+                                            , borderWidth: priority === item.level ? 2 : 0, borderColor: "#00BDD5"}}
+                                        >
+                                            <Text style={{width: "100%", height: "100%", backgroundColor: item.color, paddingTop: 5
+                                            , color: "#fff", fontSize: 17, textAlign: "center", fontWeight: "600"}}>
+                                                {item.level}
+                                            </Text>
+                                        </Pressable>
+                                    )
+                                })
+                            }
+                            
                         </View>
                     </View>
 
-                <View style={{alignItems: "center", marginTop: 40}}>
-                    <Text style={{fontSize: 35, fontWeight: "700"}}>{route.params?.action == "add" ? "ADD YOUR JOB" : "EDIT YOUR JOB"}</Text>
-
-                </View>
-                <View style={{flexDirection: "row", alignItems: "center", borderWidth: 1, borderRadius: 4, marginTop:30}}>
-                    <Image
-                        source={require("../../assets/notelist.png")}
-                        resizeMode='contain'
-                        style={{width: 35, height: 35}}
-                    />
-                    <TextInput
-                        placeholder='input your job'
-                        style={{flexGrow: 1, fontSize: 16}}
-                        value={textInput}
-                        onChangeText={setTextInput}
-                    />
-                </View>
-                <View style={{width: "100%", alignItems: "center"}}>
-                    <Pressable style={{borderRadius: 12, width: "60%", flexDirection: "row", alignItems :"center", justifyContent: "center"
-                    , marginTop: 50, backgroundColor: "#00BDD5"}}
-                        onPress={()=>handleClickFinish()}
-                    
-                    >
-                        <Text style={{fontSize: 17, color: "#fff", paddingVertical: 10, marginRight: 5}}
-                        
-                        >Finish</Text>
+                    <View style={{width: "100%", alignItems: "center", paddingHorizontal: 30}}>
+                            {
+                                isSubmit
+                                ?
+                                <ActivityIndicator style={{marginTop: 30,}} size="large"/>
+                                :
+                                <Pressable style={{borderRadius: 12, width: "60%", flexDirection: "row", alignItems :"center", justifyContent: "center"
+                                , marginTop: 50, backgroundColor: "#00BDD5"}}
+                                    onPress={()=>handleClickFinish()}
+                                    
+                                >
+                                    <Text style={{fontSize: 17, color: "#fff", paddingVertical: 10, marginRight: 5}}
+                                    
+                                    >Finish</Text>
+                                    <Ionicons name="arrow-forward" size={24} color="white" />
+                                </Pressable>
+                            }
+                    </View>
+                    <View style={{alignItems: "center", marginTop: 50, paddingHorizontal: 30}}>
                         <Image
-                            source={require("../../assets/next.png")}
+                            source={require("../../assets/note.png")}
                             resizeMode='contain'
-                            style={{width: 25, height: 25}}
+                            style={{width:250, height: 250}}
                         />
-                    </Pressable>
-                </View>
-                <View style={{alignItems: "center", marginTop: 50}}>
-                    <Image
-                        source={require("../../assets/note.png")}
-                        resizeMode='contain'
-                        style={{width:250, height: 250}}
-                    />
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    </View>
-  )
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        </View>
+    )
 }
